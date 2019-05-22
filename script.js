1eda9f0c3bfc26558ec14e682ef85d3ea376cabd
westwol <burnarg_24@hotmail.com>
2019-05-21 21:06:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Generate random room name if needed
 if (!location.hash) {
-  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  location.hash = "#vendingeo";
 }
 
 console.log(location.hash);
